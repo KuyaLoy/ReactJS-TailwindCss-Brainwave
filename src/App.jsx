da80af26a27fa7a8de65cb0d35bbde418b300ba8
Robin Tapiru <robintapiru0894@gmail.com>
2024-03-13 16:41:30 +0400

    Done: Intro, Setup, Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonGrdient from "./assets/svg/ButtonGradient";
+
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGrdient />
+    </>
+  );
+};
+export default App;
