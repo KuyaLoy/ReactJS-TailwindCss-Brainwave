314779ba03c4b761f45eaed9d81ba3ac932f78f3
Robin Tapiru <robintapiru0894@gmail.com>
2024-03-14 17:33:06 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const App = () => {
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero />
       </div>
       <ButtonGrdient />
     </>
